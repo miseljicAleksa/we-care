2727d9381058671680a4a8d23d295fcefb42c717
miseljicAleksa <aleksa.miseljic.etna@gmail.com>
2020-05-01 12:11:45 +0200

    Add TalkInformations component

--- src/components/ServiceLevel/ServiceLevel.jsx
+++ src/components/ServiceLevel/ServiceLevel.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import ReactSpeedometer from "react-d3-speedometer"
 
 import './ServiceLevel.css'
 
 export const ServiceLevel = (props) => {
+    const textColor = "#AAA"
+
     return (
         <div {...props}>
-            Service Level
+            <p id="title">SERVICE LEVEL AGREEMENT</p>
+            <ReactSpeedometer
+                needleHeightRatio={0.8}
+                height={150}
+                maxSegmentLabels={0}
+                segments={9000}
+                maxValue={100}
+                value={50}
+                currentValueText=""
+                textColor={textColor}
+                needleColor={"black"}
+                />
+            <div className="subtitleContainer">
+                <div className="red">
+                </div>
+                <p className="string">&#60;= 40</p>
+                <div className="yellow">
+
+                </div>
+                <p className="string">> 40 & &#60;=70</p>
+                <div className="green">
+
+                </div>
+                <p className="stringTwo">> 71 & &#60;= 100</p>
+            </div>
+            
+
+
         </div>
     )
 }
--- src/components/TalkInformations/TalkInformations.jsx
+++ src/components/TalkInformations/TalkInformations.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStopwatch } from '@fortawesome/free-solid-svg-icons';
 import './TalkInformations.css'
 
 export const TalkInformations = (props) => {
     return (
         <div {...props}>
-            TalkInformations
+            <section class="avaragetalk"> 
+                <FontAwesomeIcon icon={faStopwatch} size="3x" color="black" className="icons"/>
+                <h4>Average Talk Time</h4>
+                <h1>4:32</h1>
+            </section>
+            <section class="avaragewait">
+                <FontAwesomeIcon icon={faStopwatch} size="3x" color="black" className="icons"/>
+                <h4>Average Wait Time</h4>
+                <h1>3:14</h1>
+            </section>
+            <section class="longestwait">
+                <FontAwesomeIcon icon={faStopwatch} size="3x" color="black" className="icons"/>
+                <h4>Longest Wait Time</h4>
+                <h1>5:44</h1>
+            </section>
         </div>
     )
 }
