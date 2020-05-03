14c2b8fe9370c6b3a77273d77f61f5621e048589
miseljicAleksa <aleksa.miseljic.etna@gmail.com>
2020-05-03 12:22:57 +0200

    Add dinamic date object to nav and title

--- src/components/Title/Title.jsx
+++ src/components/Title/Title.jsx
@@ -3,9 +3,27 @@ import React from 'react'
 import './Title.css'
 
 export const Title = (props) => {
+    let date = new Date();
+    let hours = date.getHours();
+    let dayPeriod;
+    console.log(hours)
+    switch (true) {
+        case hours < 11:
+            dayPeriod = "Morning"
+            break;
+        case hours > 11:
+            dayPeriod = "Day"
+            break;
+        case hours > 19: 
+            dayPeriod = "Evening"
+            break;
+        default:
+            dayPeriod = "Morning"
+            break;
+    } 
     return (
         <div {...props}>
-            <p id="header">Welcome And Good Morning.</p>
+            <p id="header">Welcome And Good {dayPeriod}.</p>
             <p id="subtitle">Have a great day ahead!</p>
         </div>
     )
--- src/components/UI/Nav/NavigationBar.jsx
+++ src/components/UI/Nav/NavigationBar.jsx
@@ -3,13 +3,43 @@ import React from 'react'
  import './NavigationBar.css'
 
 export const NavigationBar = (props) => {
+    let newDate = new Date()
+    let date = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+    let hours = newDate.getHours();
+    let minutes = newDate.getMinutes();
+    let day = newDate.getDay();
+    let humanDay;
+    switch (day) {
+        case 0:
+            humanDay="Sun"
+            break;
+        case 1:
+            humanDay="Mon"
+            break;
+        case 3:
+            humanDay="Tue"
+            break;
+        case 4:
+            humanDay="Wen"
+            break;
+        case 5:
+            humanDay="Thu"
+            break;
+        case 6:
+            humanDay="Sat"
+            break;
+        default:
+            break;
+    }
     return (
         <div {...props}>
             <a  className="active" href="#title">WeCare</a>
             <a  className="title" href="#contact">Contact Center Dashboard</a>
-            <a  className="date" href="#date">28.03.2020.</a>
-            <a  className="day" href="#day">Saturday</a>
-            <a  className="time" href="#time">10:00</a>
+            <a  className="date" href="#date">{date}.{month}.{year}.</a>
+            <a  className="day" href="#day">{humanDay}</a>
+            <a  className="time" href="#time">{hours}:{minutes}</a>
         </div>
     )
 }
