18bfddef4aa98e13f9162aa94af24b5dc65a9748
Petar Matko <matko.petar@gmail.com>
2020-05-28 13:24:06 +0200

    Fix Modal for AgentStatus

--- src/components/SideBar/AgentStatusSideBar/AgentStatusSideBar.jsx
+++ src/components/SideBar/AgentStatusSideBar/AgentStatusSideBar.jsx
@@ -4,7 +4,6 @@ import '../SideBar.css'
 import {SideBarParagraph} from '../SideBarParagraph/SideBarParagraph'
 
 export const AgentStatusSideBar = (props) => {
-    
     return (
         <div {...props}>
             <div className="agentStatus">
@@ -12,9 +11,15 @@ export const AgentStatusSideBar = (props) => {
                     AGENT STATUS
                 </p>
                 {   
-                    props.dataCount ? <button className="sideBarButton" onClick={props.showModalForAgentStatus}>SEE AGENT STATUS</button> : props.agentStatus.map((txt, index) => {
-                        return(props.agentStatus.map(({fullname, status, status_time})=> <SideBarParagraph text={fullname + " " + status + " " + status_time} key={index} />))
-                    })
+                    props.dataCount ? 
+                        <button 
+                            className="sideBarButton" 
+                            onClick={props.showModalForAgentStatus}>
+                                SEE AGENT STATUS
+                        </button> : 
+                        props.agentStatus.map(({fullname, status, status_time}, index) => {
+                            return( <SideBarParagraph text={fullname + " " + status_time + " " + status} key={index} />)
+                        })
                 }
                 
             </div>
