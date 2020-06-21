963b236a62c6cedd21c39ee2b8bffc13a1bdf788
Petar Matko <matko.petar@gmail.com>
2020-06-21 18:55:50 +0200

    Move downlaod missed calls to speedometer number

--- src/components/SideBar/MissedCallsSideBar/MissedCallsSideBar.jsx
+++ src/components/SideBar/MissedCallsSideBar/MissedCallsSideBar.jsx
@@ -19,53 +19,40 @@ export const MissedCallsSideBar = (props) => {
     });
 
     const [dugme, setDugme] = useState("")
+        const handleClick = () => {
+            axios.get(`http://80.240.26.159/cdr/download-missed-calls`)
+            .then(response => {
+                FileDownload(response.data, 'nekontaktirani.txt')
+            })
+            .catch(err => console.log(err))}
 
-        return (
+
+            return (
         <div {...props}>
             <div className="missedCalls">
                 <div id="nekontaktirani">
                 <p className="title" id="nekontaktirani-title">
                     NEKONTAKTIRANI
                 </p>
-                <button onClick={()=>{
-                    axios.get(`http://80.240.26.159/cdr/download-missed-calls`)
-                    .then(response => {
-                        setDugme(response.data)
-                        FileDownload(response.data, 'nekontaktirani.txt')
-                        console.log(response.data, "DUGMEEEE calls")
-                    })
-                    .catch(err => console.log(err))}
-            }>LISTA</button>
                 </div>
-                
-                <ReactSpeedometer
-                needleHeightRatio={0.8}
-                height={150}
-                maxSegmentLabels={0}
-                segments={9000}
-                maxValue={100}
-                value={props.strelica}
-                currentValueText=""
-                needleColor={"#838D94"}
-                startColor={"#33CC33"}
-                endColor={"#FF471A"}
-                className="speedometer"
-                currentValueText={String(numberOfMissedCalls)}
-                height={180}
-                textColor={"#d8dee9"}
-                valueTextFontSize={"24px"}
-                />                {
-                    props.dataCount ?
-                        <button
-                            className="sideBarButton"
-                            onClick={props.showModalForMissedCalls}>
-                                SEE MISSED CALLS
-                        </button> : 
-                        props.missedCalls.map(({start, src, count}, index) => {
-                            return( <SideBarParagraph text={"+" + src + "      " + moment(start).format('HH:mm') + ' (' + count + ')'} key={index} />)
-                        })
-                }
-                
+                <div className='nekontaktirani-wrapper' onClick={handleClick}>
+                    <ReactSpeedometer
+                        needleHeightRatio={0.8}
+                        height={150}
+                        maxSegmentLabels={0}
+                        segments={9000}
+                        maxValue={100}
+                        value={props.strelica}
+                        needleColor={"#838D94"}
+                        startColor={"#33CC33"}
+                        endColor={"#FF471A"}
+                        className="speedometer"
+                        currentValueText={String(numberOfMissedCalls)}
+                        height={180}
+                        textColor={"#d8dee9"}
+                        valueTextFontSize={"24px"}
+                    />
+                </div>
             </div>
         </div>
     )
