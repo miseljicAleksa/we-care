05005bd983a262fbf6f578f0280496e3421e4e41
petar <petar@DB9>
2020-12-08 19:10:39 +0100

    Rename Primljeni pozivi to Dolazni pozivi

--- src/components/Calls/Calls.jsx
+++ src/components/Calls/Calls.jsx
@@ -6,7 +6,7 @@ import {Call, CallDownloadPrimljeni} from './Call/Call'
 export const Calls = (props) => {
     return (
         <div {...props}>
-            <CallDownloadPrimljeni className="Call" id="callOne" title="Primljeni pozivi" number={props.received}/>
+            <CallDownloadPrimljeni className="Call" id="callOne" title="Dolazni pozivi" number={props.received}/>
             <Call className="Call" id="callTwo" title="Odgovoreni pozivi" number={props.answered}/>
             <Call className="Call" id="callThree" title="Pozivi na čekanju" number={props.queue}/>
             <Call className="Call" id="callFour" title="Propušteni pozivi" number={props.missed}/>
